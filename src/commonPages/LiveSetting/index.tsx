bf45fb1d3f8773c4d951a7b4c86caaff21ccc2b9
Domesy <397163123@qq.com>
2021-05-31 16:25:08 +0800

    看板娘数据

--- src/commonPages/LiveSetting/index.tsx
+++ src/commonPages/LiveSetting/index.tsx
@@ -1,32 +1,45 @@
-const LiveSetting: React.FC = () => (
-  <div
-    style={{
-      width: '100%',
-      height: '100%',
-      overflow: 'hidden',
-      position: 'absolute',
-      top: 0,
-      right: 0,
-      zIndex: 200000,
-      pointerEvents: 'none',
-    }}
-  >
-    <iframe
-      title="resg"
-      src={'http://www.domesy.cn/live2d/index.html'}
+import { useEffect } from 'react';
+
+const LiveSetting: React.FC = () => {
+  useEffect(() => {
+    console.log('1');
+    window.addEventListener('message', receiveMessageFromIndex, false);
+  }, []);
+
+  const receiveMessageFromIndex = (event: any) => {
+    console.log(event.data, '00');
+  };
+
+  return (
+    <div
       style={{
         width: '100%',
-        border: '0px',
-        height: 300,
+        height: '100%',
+        overflow: 'hidden',
         position: 'absolute',
-        left: '80%',
-        bottom: 30,
-        pointerEvents: 'auto',
+        top: 0,
+        right: 0,
+        zIndex: 3,
+        pointerEvents: 'none',
       }}
-      sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
-      scrolling="auto"
-    />
-  </div>
-);
+    >
+      <iframe
+        title="resg"
+        src={'http://www.domesy.cn/live2d/index.html'}
+        style={{
+          width: '100%',
+          border: '0px',
+          height: 300,
+          position: 'absolute',
+          left: '80%',
+          bottom: 30,
+          pointerEvents: 'auto',
+        }}
+        sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
+        scrolling="auto"
+      />
+    </div>
+  );
+};
 
 export default LiveSetting;
