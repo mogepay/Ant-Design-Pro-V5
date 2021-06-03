9078f8542ac64e0227a24d25dd134f8da8882c73
Domesy <397163123@qq.com>
2021-06-03 11:33:38 +0800

    路由

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,23 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
 import type { ConnectState } from '@/models/connect';
-
 import { Card } from 'antd';
 import { connect } from 'umi';
 import { Button } from '@/components';
+import { Jump } from '@/utils';
 
 const Welcome: React.FC<any> = (props) => {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(process.env.REACT_APP_ENV);
+  }, []);
+
   return (
     <PageContainer>
       <Card>
-        <Button
-          onClick={() => {
-            console.log('1');
-          }}
-        >
-          测试
-        </Button>
+        <Button onClick={() => {}}>测试</Button>
       </Card>
       {/* <div style={{width: '100%', height: '100%'}}>
         <iframe
