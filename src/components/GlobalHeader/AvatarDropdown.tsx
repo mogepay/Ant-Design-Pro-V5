59aa0f32f69a1fa72796b055f311968e87fe4aa3
Domesy <41473452+DomeSy@users.noreply.github.com>
2021-07-06 15:25:16 +0800

    提交提交

--- src/components/GlobalHeader/AvatarDropdown.tsx
+++ src/components/GlobalHeader/AvatarDropdown.tsx
@@ -43,9 +43,24 @@ class AvatarDropdown extends React.Component<GlobalHeaderRightProps> {
         avatar: '',
         name: '',
       },
+      menu,
     } = this.props;
     const menuHeaderDropdown = (
-      <Menu className={styles.menu} selectedKeys={[]} onClick={(e: any) => this.onMenuClick(e)}>
+      <Menu className={styles.menu} selectedKeys={[]} onClick={this.onMenuClick}>
+        {menu && (
+          <Menu.Item key="center">
+            <UserOutlined />
+            个人中心
+          </Menu.Item>
+        )}
+        {menu && (
+          <Menu.Item key="settings">
+            <SettingOutlined />
+            个人设置
+          </Menu.Item>
+        )}
+        {menu && <Menu.Divider />}
+
         <Menu.Item key="logout">
           <LogoutOutlined />
           退出登录
